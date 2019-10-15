60755ab02ab2f2e2a8f655dd982503e4c584254e
fzk27 <wnz27@icloud.com>
2019-10-15 10:08:35 +0800

    update

---
+++ Python_Generate/test.py
@@ -0,0 +1,17 @@
+def pivotIndex(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 空数组
+        if not nums:
+            return -1
+        
+        sum_nums = sum(nums)
+        left = 0        
+        # 遍历比较
+        for i in range(len(nums)):
+            if left * 2 == sum_nums - nums[i]:
+                return i
+            left += nums[i]
+        return -1
