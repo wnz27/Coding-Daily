28f58fabf1f184eae2037625ca77d56e8cb5b739
fzk27 <wnz27@icloud.com>
2019-10-24 15:57:20 +0800

    update

--- Python_Generate/test.py
+++ Python_Generate/test.py
@@ -10,10 +10,23 @@ def pivotIndex(nums):
             return -1
         
         sum_nums = sum(nums)
-        left = 0        
+        left = 0        # 左起求的和
         # 遍历比较
         for i in range(len(nums)):
             if left * 2 == sum_nums - nums[i]:
                 return i
             left += nums[i]
         return -1
+# 值类型互不影响，开辟了新的内存空间
+a = 1
+b = a
+a = 2
+print(b)
+print(id(a), id(b))
+
+# 引用类型影响，他们指向同一个对象
+c = {"age":2}
+d = c
+d["age"] = 3
+print(c.get("age"))
+print(id(c), id(d))
