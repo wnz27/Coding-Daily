7a58a1effdf629f728a598684ce7dc35f28fd623
fzk27 <wnz27@icloud.com>
2020-02-18 00:41:49 +0800

    update

--- DesignPattern/test.py
+++ DesignPattern/test.py
@@ -1,2 +1,22 @@
-a = {'python':True}
-print(list(a.values()))
+b = [1,7,5,9,2,12,3]
+a = range(len(b))
+c = {k:v for k,v in zip(a, b)}
+print(c)
+result = []
+for value in b:
+    if (value + 2) in b:
+        result.append((value, value+2))
+print(result)
+
+d = "asdfasdf asdfafads asdfasdf"
+s = d.split(' ')
+g = '%20'.join(s)
+print(g)
+print(max(1, 1))
+
+print("fzk".join(" 13 13 21 ".split(" ")))
+
+# from numpy import random
+# lala = random.randint(1,5, 10)
+# print(lala)
+
