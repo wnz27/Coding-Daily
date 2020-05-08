9b1e48784ea946c2cb8304a391094143cb8eb35c
fzk27 <wnz27@icloud.com>
2020-05-09 02:06:18 +0800

    update

---
+++ test.py
@@ -0,0 +1,15 @@
+'''
+@Author: 27
+@LastEditors: 27
+@Date: 2020-05-08 01:55:10
+@LastEditTime: 2020-05-08 02:04:35
+@FilePath: /Coding-Daily/test.py
+@description: type some description
+'''
+import random
+
+a = random.sample(range(0, 200), 100)
+print(a)
+
+b = random.randint(0, 10)
+print(b)
