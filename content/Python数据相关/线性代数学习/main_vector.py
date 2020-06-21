b75bf04b74a44d6f6232412386165e48dd382e42
fzk27 <wnz27@icloud.com>
2020-06-21 11:39:35 +0800

    fix import

--- content/Python数据相关/线性代数学习/main_vector.py
+++ content/Python数据相关/线性代数学习/main_vector.py
@@ -1,7 +1,15 @@
+'''
+@Author: 27
+@LastEditors: 27
+@Date: 2020-03-29 23:12:42
+@LastEditTime: 2020-06-21 11:39:17
+@FilePath: /Coding-Daily/content/Python数据相关/线性代数学习/main_vector.py
+@description: type some description
+'''
 # -*- coding:utf-8 -*-
 # Create by 27
 # @Time : 2020/3/29 23:12
-from content.Python数据相关.线性代数学习.playLA.Vector import Vector
+from playLA.Vector import Vector
 
 __author__ = '27'
 
