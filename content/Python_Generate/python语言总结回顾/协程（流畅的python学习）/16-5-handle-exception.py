acce42744466c248faeb784eeddec268a67d65db
fzk27 <wnz27@icloud.com>
2020-06-26 16:09:15 +0800

    协程与异常小demo

--- content/Python_Generate/python语言总结回顾/协程（流畅的python学习）/16-5-handle-exception.py
+++ content/Python_Generate/python语言总结回顾/协程（流畅的python学习）/16-5-handle-exception.py
@@ -3,3 +3,26 @@
 # @Time : 2020/4/3 15:36
 __author__ = '27'
 
+
+class DemoException(Exception):
+    """为这次演示定义的异常类型。"""
+    pass
+
+
+def demo_exc_handling():
+    print('-> coroutine started')
+    while True:
+        try:
+            x = yield
+        except DemoException:   # ➊
+            print('*** DemoException handled. Continuing...')
+        else:  # ➋
+            print('-> coroutine received: {!r}'.format(x))
+    raise RuntimeError('This line should never run.')  # ➌
+
+"""
+❶ 特别处理 DemoException 异常。
+❷ 如果没有异常，那么显示接收到的值。
+❸ 这一行永远不会执行。
+"""
+
