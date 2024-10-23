05cd60a0d02fb06927628bb8975798732a137d5f
wnz27 <wnz27@icloud.com>
2024-10-23 10:19:57 +0800

    feat: update leran

---
+++ content/lua/exit.lua
@@ -0,0 +1,9 @@
+--[[
+Author: 27
+LastEditors: 27
+Date: 2024-10-22 17:59:36
+LastEditTime: 2024-10-22 17:59:37
+FilePath: /Coding-Daily/content/lua/exit.lua
+description: type some description
+--]]
+os.exit(3)
